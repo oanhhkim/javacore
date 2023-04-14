8fa4a894fa73435ad7d412149afbfe8998cb6abc
oanhhkim <kimoanhh0103@gmail.com>
2023-04-14 21:33:01 +0700

    fix

--- Eratosthenes.java
+++ Eratosthenes.java
@@ -1,31 +1,41 @@
 import java.util.Scanner;
 
 public class Eratosthenes {
+
   Scanner scanner = new Scanner(System.in);
-  public void input(){
+  int[] prime = new int[1000001];
+
+  public int input() {
     System.out.print("n = ");
     int n = scanner.nextInt();
+    return n;
   }
-  public void eratosthenes(int check){
-    int n = 1000;
-    check = new boolean[n];
-    for(int i = 2; i<=n; i++){
-      check[i] = true;
+
+  public void eratosthenes(int n) {
+    for (int i = 0; i <= n; i++) {
+      prime[i] = 1;
     }
-    for(int i = 2; i<=n; i++){
-      if(check[i]==true){
-        for(int j =2*i; j<=n; j+=i){
-          check[j] = false;
+    prime[0] = prime[1] = 0;
+    for (int i = 2; i <= n; i++) {
+      if (prime[i] == 1) {
+        for (int j = i * i; j <= 1000001; j += i) {
+          prime[j] = 0;
         }
       }
     }
   }
 
-  public static void main(String[] args) {
-    for(int i = 2; i<n; i++){
-      if(check[i]==true)
+  public void display(int n) {
+    for (int i = 0; i <= n; i++) {
+      if (prime[i] == 1) {
+        System.out.print(i + " ");
+      }
     }
   }
 
-
+  public static void main(String[] args) {
+    Eratosthenes sieve = new Eratosthenes();
+    int n = sieve.input();
+    sieve.display(n);
+  }
 }
