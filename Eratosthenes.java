28aa1dc206c3aa79ef118945ffaddc783005cce6
oanhhkim <kimoanhh0103@gmail.com>
2023-04-14 21:46:42 +0700

    fix

--- Eratosthenes.java
+++ Eratosthenes.java
@@ -16,9 +16,9 @@ public class Eratosthenes {
       prime[i] = 1;
     }
     prime[0] = prime[1] = 0;
-    for (int i = 2; i <= n; i++) {
+    for (int i = 2; i <= Math.sqrt(n); i++) {
       if (prime[i] == 1) {
-        for (int j = i * i; j <= 1000001; j += i) {
+        for (int j = i * i; j <=n; j += i) {
           prime[j] = 0;
         }
       }
